dc6b38250bb23eb181e7ae1160b8f9e3a1ca791b
Tim Farrow <tim@jetstechnologies.com>
2020-07-07 17:04:42 -0600

    fix(env): fix env

--- src/assets/js/env.js
+++ src/assets/js/env.js
@@ -1,7 +1,5 @@
 const requiredKeys = ['API_URL', 'API_PROTOCOL', 'NODE_ENV', 'FIREBASE_API_KEY', 'FIREBASE_AUTH_DOMAIN', 'FIREBASE_DATABASE_URL', 'FIREBASE_PROJECT_ID', 'FIREBASE_STORAGE_BUCKET', 'FIREBASE_MESSAGING_SENDER_ID', 'FIREBASE_APP_ID', 'FIREBASE_MEASUREMENT_ID'];
 
-console.log( process.env );
-
 const missingKeys = requiredKeys
 	.map( a => `VUE_APP_${a}` )
 	.filter( key => !process.env[key] )
--- src/lib/db/conf.js
+++ src/lib/db/conf.js
@@ -1,38 +1,28 @@
 import firebase from 'firebase';
 
-require( 'dotenv' ).config( { path : '.env' } );
+import env from '@/assets/js/env';
 
-console.log( process.env );
-
-const apiKey            = process.env.VUE_APP_FIREBASE_API_KEY;
-const authDomain        = process.env.VUE_APP_FIREBASE_AUTH_DOMAIN;
-const databaseURL       = process.env.VUE_APP_FIREBASE_DATABASE_URL;
-const projectId         = process.env.VUE_APP_FIREBASE_PROJECT_ID;
-const storageBucket     = process.env.VUE_APP_FIREBASE_STORAGE_BUCKET;
-const messagingSenderId = process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID;
-const appId             = process.env.VUE_APP_FIREBASE_APP_ID;
-const measurementId     = process.env.VUE_APP_FIREBASE_MEASUREMENT_ID;
+const {
+	FIREBASE_API_KEY             : apiKey,
+	FIREBASE_AUTH_DOMAIN         : authDomain,
+	FIREBASE_DATABASE_URL        : databaseURL,
+	FIREBASE_PROJECT_ID          : projectId,
+	FIREBASE_STORAGE_BUCKET      : storageBucket,
+	FIREBASE_MESSAGING_SENDER_ID : messagingSenderId,
+	FIREBASE_APP_ID              : appId,
+	FIREBASE_MEASUREMENT_ID      : measurementId,
+} = env;
 
 const firebaseConfig = {
-	// apiKey,
-	// authDomain,
-	// databaseURL,
-	// projectId,
-	// storageBucket,
-	// messagingSenderId,
-	// appId,
-	// measurementId,
-	apiKey            : 'AIzaSyA6YXrXm3QrGT2swuuXXOsDZpj38pMbrw8',
-	authDomain        : 'musd-equity-report.firebaseapp.com',
-	databaseURL       : 'https://musd-equity-report.firebaseio.com',
-	projectId         : 'musd-equity-report',
-	storageBucket     : 'musd-equity-report.appspot.com',
-	messagingSenderId : '837648574102',
-	appId             : '1:837648574102:web:b50dcb2e70f554679984b7',
-	measurementId     : 'G-SV1SBB28L7'
+	apiKey,
+	authDomain,
+	databaseURL,
+	projectId,
+	storageBucket,
+	messagingSenderId,
+	appId,
+	measurementId,
 };
-
-console.log( firebaseConfig );
 
 // Initialize Firebase
 firebase.initializeApp( firebaseConfig );
