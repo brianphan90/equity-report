8446765bc3829ea8373fe6e79acc686606a9be59
Alejandro Morales <43051685+alemor10@users.noreply.github.com>
2020-11-17 17:40:15 -0800

    update(attendance graphs): started computed/method

--- src/routes/index/climate-and-engagement/staff/Graphs/TimelineGraph.vue
+++ src/routes/index/climate-and-engagement/staff/Graphs/TimelineGraph.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-
+	svg(ref='svg')
 </template>
 
 <script>
@@ -14,17 +14,133 @@ export default {
 
 	props : {
 		data : {
-			type    : Object,
-			default : () => ( {} )
+			type    : Array,
+			default : () => ( [] )
 		}
 	},
 
 	data : () => ( {
-		axisIndicators : null,
-		barGroups      : null,
+		axisIndicators  : null,
+		barGroups       : null,
+		xAxisLabels     : null,
+		barLabelSpacing : 5
 	} ),
 
 	computed : {
+		numOfBars() {
+			return 12;
+		},
+		months() {
+			return [
+				'July',
+				'August',
+				'September',
+				'October',
+				'November',
+				'December',
+				'January',
+				'February',
+				'March',
+				'April',
+				'May',
+				'June',
+			];
+		},
+		range() {
+			const { data } = this;
+			const values = data.map( d => d.value );
+			const range = this.getDataRange( values, false );
+
+			return {
+				min : range.start,
+				max : range.end
+			};
+		},
+	},
+
+	mounted() {
+		this.init( this.$refs.svg );
+	},
+
+	methods : {
+		draw() {
+			this.drawMonths( this.months );
+			this.barGroups = this.createBarGroups( this.data );
+
+			this.xAxisLabels = this.drawXAxisLabels( this.barGroups );
+			// draw labels
+			const lineIndicators = this.drawAxisIndicators( {
+				range    : this.range,
+				axis     : 'y',
+				postChar : '',
+				lines    : {
+					spaceBetweenLabelsAndLines : 5,
+					numberOfIndicators         : 12,
+				},
+				dayColor   : colors.grey,
+				nightColor : colors.white,
+			} );
+		},
+		drawMonths( months ) {
+			console.log( months );
+		},
+
+		createBarGroups( data ) {
+			return this.canvas
+				.selectAll( '.bar-groups' )
+				.data( data )
+				.enter()
+				.append( 'g' )
+				.attr( 'class', 'bar-groups' );
+		},
+
+		drawXAxisLabels( barGroups ) {
+			const xAxisLabels = barGroups
+				.append( 'text' )
+				.attr( 'class', 'x-axis-labels' )
+				.attr( 'x', ( d, i ) => this.getTextX( i ) )
+				.attr( 'y', this.t + this.ah )
+				.attr( 'text-anchor', 'middle' )
+				.attr( 'dominant-baseline', 'start' )
+				.style( 'font-size', '12px' )
+				.text( d => d.label );
+
+			this.changeWithMode( {
+				nodes   : xAxisLabels,
+				options : {
+					day : {
+						style : [
+							['fill', colors.grey],
+						]
+					},
+					night : {
+						style : [
+							['fill', colors.white]
+						],
+					},
+				},
+			} );
+			const barLabelNodes = barGroups.selectAll( '.x-axis-labels' );
+			const barLabelWidths = Array.from( barLabelNodes._groups[0] ).map( a => a.getBBox().height );
+			const biggestBarLabel = Math.max( ...barLabelWidths );
+			const spaceBetweenBarAndXAxis = 2;
+
+			this.updateDims( {
+				b : biggestBarLabel + spaceBetweenBarAndXAxis
+			} );
+
+			return xAxisLabels;
+		},
+		getTextX( i ) {
+			const {
+				l,
+				barWidth,
+				barSpacing
+			} = this;
+
+			return ( l + ( barWidth * i ) + ( barSpacing * i ) + ( barWidth / 2 ) ) + ( barSpacing / 2 );
+		},
+
 	}
 };
 </script>
